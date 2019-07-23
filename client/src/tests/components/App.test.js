f9028b5cb49dff7f9a40adaa8eb1449eb09d2bb4
aapetsi <apetsiampiah@gmail.com>
2019-07-23 10:39:00 +0000

    create test branch to fix issue

--- client/src/tests/components/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "../../enzyme";
-
-import App from "../../components/App";
-
-test("should render App component correctly", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
--- index.js
+++ index.js
@@ -106,7 +106,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
