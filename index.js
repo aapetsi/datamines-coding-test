abc8dd8db707e4ae3c4c3153602db81b7145e248
aapetsi <apetsiampiah@gmail.com>
2019-07-23 05:43:28 +0000

    prepare for deploy

--- index.js
+++ index.js
@@ -97,6 +97,14 @@ app.get(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
